059dc5e52d62512092e33602eb133f60363c0123
akarki7 <karkiaabishkar@gmail.com>
2022-11-17 15:15:19 +0545

    Finished autoamtic upload to database using script

--- webscraper/utils_scraper.py
+++ webscraper/utils_scraper.py
@@ -1,5 +1,21 @@
 import pandas as pd
-import shutil
+from dotenv import dotenv_values
+from sqlalchemy import create_engine
+from urllib.parse import quote_plus
+
+config = dotenv_values(".env")
+
+host_db = config["HOST_db"]
+user_db = config["USER_db"]
+passw_db = config["PASSWORD_db"]
+name_db = config["DATABASE_db"]
+
+
+def connect_to_database():
+    url = f"mssql+pymssql://{user_db}:%s@{host_db}/{name_db}" % quote_plus(passw_db)
+    print(url)
+    eng = create_engine(url)
+    return eng
 
 
 def create_template_A_and_L():
@@ -44,8 +60,14 @@ def add_year_and_monthname(file, sheet, year, month_name):
 def add_values(file, sheet, values, new_file, new_sheet):
     workbook = pd.read_excel(file, sheet_name=sheet)
 
-    workbook["Value"] = values
+    workbook["Amount"] = values
 
     writer = pd.ExcelWriter(new_file, engine="xlsxwriter")
     workbook.to_excel(writer, index=False, sheet_name=new_sheet)
     writer.close()
+
+
+def bulk_insert_sql(file, sheet, table):
+    conn = connect_to_database()
+    data = pd.read_excel(file, sheet_name=sheet)
+    data.to_sql(table, conn, if_exists="append", index=False)
